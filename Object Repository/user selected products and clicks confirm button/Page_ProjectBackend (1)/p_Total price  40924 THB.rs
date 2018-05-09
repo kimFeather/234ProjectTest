27e152b34cf44992ce00ab0869ebd9c351bcdaf1
kimFeather <kuribohw13@gmail.com>
2018-05-09 22:27:42 +0700

    total pricce 40924 thb in  user selected products

---
+++ Object Repository/user selected products and clicks confirm button/Page_ProjectBackend (1)/p_Total price  40924 THB.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>p_Total price  40924 THB</name>
+   <tag></tag>
+   <elementGuidId>01af9862-e7c5-465b-9f98-af71f5913469</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Total price:  40,924 THB</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;add-row&quot;)/div[@class=&quot;col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;offset-8 col-4&quot;]/p[1]</value>
+   </webElementProperties>
+</WebElementEntity>
