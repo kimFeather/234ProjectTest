e45cdef4e893a7e4f58b62f4d7a023c2b9ea4d0d
kimFeather <kuribohw13@gmail.com>
2018-05-09 21:53:41 +0700

    user clicks logout test suites

---
+++ Test Suites/user clicks logout.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>user clicks logout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>edf43ee0-e907-4299-9236-b446e9d448f5</testSuiteGuid>
+   <testCaseLink>
+      <guid>b634932d-d1fa-4435-b4a4-f3bb1ac218d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/user clicks logout</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>150e0ce7-12ef-49e7-8eb3-ff0943ef5506</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/user clicks logout</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>150e0ce7-12ef-49e7-8eb3-ff0943ef5506</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>7292576d-531d-44a5-a0ba-15815ba22ed5</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>150e0ce7-12ef-49e7-8eb3-ff0943ef5506</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>37f8894d-472a-4677-8471-c657ada0a07c</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
