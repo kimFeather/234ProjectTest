8b51473f8881d65cb883e2fad05e8965349fcf66
kimFeather <kuribohw13@gmail.com>
2018-05-09 21:52:31 +0700

    user products,cars test suites

---
+++ Test Suites/user products,cars.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>user products,cars</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6ffcad18-66d4-4f2d-becc-6b9e35488bc6</testSuiteGuid>
+   <testCaseLink>
+      <guid>adfa7124-cd38-42d5-9c7d-73ed57ea9980</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/user products,cars</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>c333ce07-f038-4b08-97c0-db46adc30c39</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/user products,cars</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>c333ce07-f038-4b08-97c0-db46adc30c39</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>5da9bf97-b52e-4183-a201-f1aa9c669cc1</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c333ce07-f038-4b08-97c0-db46adc30c39</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>328d9a2d-d2f3-4158-b941-834dc02e53dd</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
