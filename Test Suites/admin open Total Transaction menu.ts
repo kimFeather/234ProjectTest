f717934edbc5f25d060576800b5755b210ec5ac2
kimFeather <kuribohw13@gmail.com>
2018-05-09 23:30:15 +0700

    admin open total transaction menu test suites

---
+++ Test Suites/admin open Total Transaction menu.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>admin open Total Transaction menu</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-09T15:47:00</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>88b64340-9afa-47e6-8425-03eb81076f90</testSuiteGuid>
+   <testCaseLink>
+      <guid>ba0f3013-ef69-4e89-b8e7-a20e8680ade5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/admin open Total Transaction menu</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>2bb8eae9-b518-4fb2-8667-515402e124fb</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/admin open Total Transaction menu</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>2bb8eae9-b518-4fb2-8667-515402e124fb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>728a231d-d6b3-41eb-a7ec-307b65607459</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>2bb8eae9-b518-4fb2-8667-515402e124fb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>b9936929-2119-41dd-b8d7-55b10e237472</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
