b2da62e4d73ddae86d13708ab566d11e622b1c54
kimFeather <kuribohw13@gmail.com>
2018-05-09 23:28:59 +0700

    admin total trasaction test suites

---
+++ Test Suites/admin total transaction.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>admin total transaction</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9d79c82e-821e-4fc7-afe3-0c6c06aecc84</testSuiteGuid>
+   <testCaseLink>
+      <guid>c80ef3eb-5490-4069-8c13-78333cf5f8ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/admin total transaction</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>c1800d5e-a151-4de3-974f-84bb342a2be7</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/admin total transaction</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>c1800d5e-a151-4de3-974f-84bb342a2be7</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>admin_username</value>
+         <variableId>174fcbcb-948b-41ac-b266-8bbbde513db4</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c1800d5e-a151-4de3-974f-84bb342a2be7</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>admin_password</value>
+         <variableId>561d83f8-daa3-44a0-8d0f-2568285519ee</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c1800d5e-a151-4de3-974f-84bb342a2be7</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>user_username</value>
+         <variableId>e138c50b-efe3-4a29-9af5-3d538ee3e195</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c1800d5e-a151-4de3-974f-84bb342a2be7</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>user_password</value>
+         <variableId>79aa7794-5377-4f52-97b5-ae9a179311c1</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
