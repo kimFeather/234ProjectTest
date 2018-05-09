a43d30e2865855909fa9054a1eb78eab2e829853
kimFeather <kuribohw13@gmail.com>
2018-05-09 21:55:31 +0700

    user clicked at the add to cart button test suits

---
+++ Test Suites/user clicked at the add to cart button.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>user clicked at the add to cart button</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>edca192a-99d1-4ed3-99a8-5dfc352fb512</testSuiteGuid>
+   <testCaseLink>
+      <guid>be8b4db0-8d5c-4f90-93ee-3ff0d11f5701</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/user clicked at the add to cart button</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>0011b3b2-6382-46a3-b3de-ecc26efd7588</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/user clicked at the add to cart button</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>0011b3b2-6382-46a3-b3de-ecc26efd7588</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>cb3f04ab-afa5-4849-bebd-637cd44bbfb0</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>0011b3b2-6382-46a3-b3de-ecc26efd7588</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>c68fd595-7f92-458e-a30c-36d05174fac4</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
