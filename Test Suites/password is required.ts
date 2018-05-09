f153102aba5caa689c7f444577511ae33323f24d
kimFeather <kuribohw13@gmail.com>
2018-05-09 22:24:27 +0700

    password is required test suites

---
+++ Test Suites/password is required.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>password is required</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7e5ac3e5-cb0d-4ce0-9796-8061e4e91a41</testSuiteGuid>
+   <testCaseLink>
+      <guid>0cabbfb5-5cd3-4ad9-85eb-8577c730b833</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/password is required</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>ff866c7c-b376-480b-9172-ed42951abb39</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/password is required</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>ff866c7c-b376-480b-9172-ed42951abb39</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>34cea8b7-a53a-41a0-84dd-a9fbfbbc7293</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
