7490d5e49d1117b385ffd26e966bdc622747e5ed
kimFeather <kuribohw13@gmail.com>
2018-05-09 22:48:10 +0700

    incorrect id,pass test suits

---
+++ Test Suites/incorrect id,pass.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>incorrect id,pass</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>55cbb301-ed96-4942-a715-ab8c3d6094ec</testSuiteGuid>
+   <testCaseLink>
+      <guid>91e2120b-9049-44ea-8a59-6ebe8323b9eb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/incorrect id,pass</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>36a28fe6-52fd-4829-a348-8a61909920fc</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/incorrect id,pass</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>36a28fe6-52fd-4829-a348-8a61909920fc</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>52eb966e-8d02-4a86-b9af-3b82a809662e</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>36a28fe6-52fd-4829-a348-8a61909920fc</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>c6834c1e-516c-4c2a-8a6f-30535546752c</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
